6ab31e8f7da202e13b75c37cb3a185e9c8979443
Aron Pachl <pachl.aron@gmail.om>
2020-03-10 11:22:22 +0100

    corrected sql file added

--- data/boards.sql
+++ data/boards.sql
@@ -1,72 +1,69 @@
-CREATE TABLE IF NOT EXISTS board(
-    id serial NOT NULL primary key ,
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS pk_cards_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_status_id_cards CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_board_id_cards CASCADE;
+ALTER TABLE IF EXISTS ONLY public.board DROP CONSTRAINT IF EXISTS pk_board_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.statuses DROP CONSTRAINT IF EXISTS pk_statuses_id CASCADE;
+
+DROP TABLE IF EXISTS public.board;
+CREATE TABLE board(
+    id serial NOT NULL,
     title text
 );
 
-INSERT INTO board (title) VALUES ('Board 1');
-INSERT INTO board (title) VALUES ('Board 2');
-
-CREATE TABLE IF NOT EXISTS cards (
-    id serial NOT NULL primary key ,
+DROP TABLE IF EXISTS public.cards;
+CREATE TABLE cards (
+    id serial NOT NULL,
     board_id integer,
     title text,
     status_id integer,
-    order boolean
-
+    "order" boolean
 );
 
-ALTER TABLE cards
-    ADD CONSTRAINT fk_board_id_cards
-        foreign key (board_id) references board (id) on delete cascade,
-    ADD CONSTRAINT fk_status_id_cards
-        foreign key (status_id) references statuses(id) on delete cascade;
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'new card 1', 0, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'new card 2', 0, 1);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'in progress card', 1, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'planning', 2, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'done card 1', 3, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (1, 'done card 1', 3, 1);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'new card 1', 0, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'new card 2', 0, 1);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'in progress card', 1, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'planning', 2, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'done card 1', 3, 0);
-
-INSERT INTO cards (board_id, title, status_id, "order")
-VALUES (2, 'done card 1', 3, 1);
-
-
-CREATE TABLE IF NOT EXISTS statuses (
-    id serial NOT NULL primary key,
+DROP TABLE IF EXISTS public.statuses;
+CREATE TABLE statuses (
+    id serial,
     title text
 );
+ALTER TABLE ONLY board
+    ADD CONSTRAINT pk_board_id PRIMARY KEY (id);
 
-INSERT INTO statuses (title) VALUES ('new');
+ALTER TABLE ONLY statuses
+    ADD CONSTRAINT pk_statuses_id PRIMARY KEY (id);
 
-INSERT INTO statuses (title) VALUES ('in progress');
+ALTER TABLE cards
+    ADD CONSTRAINT pk_cards_id PRIMARY KEY (id),
+    ADD CONSTRAINT pk_board_id
+        foreign key (board_id) references board (id) on delete cascade,
+    ADD CONSTRAINT pk_statuses_id
+        foreign key (status_id) references statuses(id) on delete cascade;
 
-INSERT INTO statuses (title) VALUES ('testing');
-
-INSERT INTO statuses (title) VALUES ('done');
+INSERT INTO board (title) VALUES ('Board 1');
+INSERT INTO board (title) VALUES ('Board 2');
+INSERT INTO statuses (id, title) VALUES (0, 'new');
+INSERT INTO statuses (id, title) VALUES (1, 'in progress');
+INSERT INTO statuses (id, title) VALUES (2, 'testing');
+INSERT INTO statuses (id, title) VALUES (3, 'done');
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'new card 1', 0, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'new card 2', 0, true);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'in progress card', 1, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'planning', 2, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'done card 1', 3, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (1, 'done card 1', 3, true);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'new card 1', 0, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'new card 2', 0, true);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'in progress card', 1, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'planning', 2, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'done card 1', 3, false);
+INSERT INTO cards (board_id, title, status_id, "order")
+VALUES (2, 'done card 1', 3, true);
