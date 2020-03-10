5d645997c223a5c45db766cdabed1222d64ec790
Viktor Bocz <beviktor95@gmail.com>
2020-03-10 16:02:21 +0100

    Handling new queries(all boards, statuses)

--- data_handler.py
+++ data_handler.py
@@ -12,19 +12,23 @@ def get_card_status(status_id):
 
 
 def get_boards():
-    """
-    Gather all boards
-    :return:
-    """
-    return persistence.get_boards(force=True)
+    return persistence.collect_all_board()
 
 
 def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards
+    return persistence.get_cards_by_board_id(board_id)
+
+
+def get_statuses_from_persistence():
+    return persistence.get_statuses
+
+
+# def get_cards_for_board(board_id):
+#     persistence.clear_cache()
+#     all_cards = persistence.get_cards()
+#     matching_cards = []
+#     for card in all_cards:
+#         if card['board_id'] == str(board_id):
+#             card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
+#             matching_cards.append(card)
+#     return matching_cards
