76413e902ee56bca54e83fc9a699e09c44b0e913
Steven Trautmann <steven.trautmann1@gmail.com>
2020-03-11 15:10:19 +0100

    trying to make the display work

--- data_handler.py
+++ data_handler.py
@@ -12,23 +12,31 @@ def get_card_status(status_id):
 
 
 def get_boards():
-    statuses = persistence.get_statuses()
     all_board = persistence.collect_all_board()
     for board in all_board:
-        status_number = 1
-        for status in statuses:
-            board[f'status{status_number}'] = status['status']
-            status_number += 1
+        cards = persistence.get_card_status_board(board['id'])
+        board['status'] = get_statuses_for_specific_board(board['id'])
+        for stat in board['status']:
+            board[stat] = [card['card'] for card in cards if card['status'] == stat]
+
+    print(all_board)
     return all_board
 
 
+def get_statuses_for_specific_board(board_id):
+    return [persistence.get_status_title_by_id(card_status['status_id'])['title'] for card_status
+            in persistence.get_all_cards_status_id_for_a_board(board_id)]
+
+
 def get_cards_for_board(board_id):
-    return persistence.get_cards_by_board_id(board_id)
+    status_ids = persistence.all_status_ids_of_a_board(board_id)
+
+    persistence.get_card_name_by_status_id(status_id, board_id)
+    return
 
 
 def get_statuses_from_persistence():
     return persistence.get_statuses
-
 
 # def get_cards_for_board(board_id):
 #     persistence.clear_cache()
