19f66c556628f31b6ca3fcb2678a11d7ae39d851
Viktor Bocz <beviktor95@gmail.com>
2020-03-12 11:22:04 +0100

    Init users table

--- data/boards.sql
+++ data/boards.sql
@@ -1,31 +1,58 @@
-ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS pk_cards_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_status_id_cards CASCADE;
-ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_board_id_cards CASCADE;
-ALTER TABLE IF EXISTS ONLY public.board DROP CONSTRAINT IF EXISTS pk_board_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.statuses DROP CONSTRAINT IF EXISTS pk_statuses_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards
+    DROP CONSTRAINT IF EXISTS pk_cards_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards
+    DROP CONSTRAINT IF EXISTS fk_status_id_cards CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards
+    DROP CONSTRAINT IF EXISTS fk_board_id_cards CASCADE;
+ALTER TABLE IF EXISTS ONLY public.board
+    DROP CONSTRAINT IF EXISTS pk_board_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.statuses
+    DROP CONSTRAINT IF EXISTS pk_statuses_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.users
+    DROP CONSTRAINT IF EXISTS pk_users_id CASCADE;
 
 DROP TABLE IF EXISTS public.board;
-CREATE TABLE board(
-    id serial NOT NULL,
-    title text
+CREATE TABLE board
+(
+    id      serial NOT NULL,
+    title   text,
+    user_id integer
 );
 
 DROP TABLE IF EXISTS public.cards;
-CREATE TABLE cards (
-    id serial NOT NULL,
-    board_id integer,
-    title text,
+CREATE TABLE cards
+(
+    id        serial NOT NULL,
+    board_id  integer,
+    title     text,
     status_id integer,
-    "order" boolean
+    "order"   boolean,
+    user_id   integer
 );
 
 DROP TABLE IF EXISTS public.statuses;
-CREATE TABLE statuses (
-    id serial,
+CREATE TABLE statuses
+(
+    id    serial,
     title text
 );
+
+DROP TABLE IF EXISTS public.users;
+CREATE TABLE users
+(
+    id       serial,
+    name     text,
+    password text,
+    registration_date timestamp without time zone
+);
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT pk_users_id PRIMARY KEY (id);
+
 ALTER TABLE ONLY board
-    ADD CONSTRAINT pk_board_id PRIMARY KEY (id);
+    ADD CONSTRAINT pk_board_id PRIMARY KEY (id),
+    ADD CONSTRAINT pk_users_id
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY statuses
     ADD CONSTRAINT pk_statuses_id PRIMARY KEY (id);
@@ -35,14 +62,22 @@ ALTER TABLE cards
     ADD CONSTRAINT pk_board_id
         foreign key (board_id) references board (id) on delete cascade,
     ADD CONSTRAINT pk_statuses_id
-        foreign key (status_id) references statuses(id) on delete cascade;
+        foreign key (status_id) references statuses (id) on delete cascade,
+    ADD CONSTRAINT pk_users_id
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 
-INSERT INTO board (title) VALUES ('Board 1');
-INSERT INTO board (title) VALUES ('Board 2');
-INSERT INTO statuses (id, title) VALUES (0, 'new');
-INSERT INTO statuses (id, title) VALUES (1, 'in progress');
-INSERT INTO statuses (id, title) VALUES (2, 'testing');
-INSERT INTO statuses (id, title) VALUES (3, 'done');
+INSERT INTO board (title)
+VALUES ('Board 1');
+INSERT INTO board (title)
+VALUES ('Board 2');
+INSERT INTO statuses (id, title)
+VALUES (0, 'new');
+INSERT INTO statuses (id, title)
+VALUES (1, 'in progress');
+INSERT INTO statuses (id, title)
+VALUES (2, 'testing');
+INSERT INTO statuses (id, title)
+VALUES (3, 'done');
 INSERT INTO cards (board_id, title, status_id, "order")
 VALUES (1, 'new card 1', 0, false);
 INSERT INTO cards (board_id, title, status_id, "order")
@@ -67,3 +102,4 @@ INSERT INTO cards (board_id, title, status_id, "order")
 VALUES (2, 'done card 1', 3, false);
 INSERT INTO cards (board_id, title, status_id, "order")
 VALUES (2, 'done card 1', 3, true);
+
