11a7cbe0447ff61f183a2c6ff1f9059b7ca9ecb6
Viktor Bocz <beviktor95@gmail.com>
2020-03-25 14:02:12 +0100

    Change status persistence level

--- persistence.py
+++ persistence.py
@@ -42,15 +42,6 @@ def get_login_data(cursor, username):
         data = datadict
         return data
 
-# @database_common.connection_handler
-# def get_cards_by_board_id(cursor, board_id):
-#     cursor.execute("""
-#     SELECT title
-#     FROM cards
-#     WHERE board_id = %(board_id)s
-#     """, {'board_id': board_id})
-#     return cursor.fetchall()
-
 
 @database_common.connection_handler
 def get_statuses(cursor):
@@ -248,34 +239,10 @@ def delete_card_sql(cursor, card_id):
     """, {'card_id': card_id})
 
 
-# _cache = {}  # We store cached data in this dict to avoid multiple file readings
-#
-#
-# def _get_data(data_type, file, force):
-#     """
-#     Reads defined type of data from file or cache
-#     :param data_type: key where the data is stored in cache
-#     :param file: relative path to data file
-#     :param force: if set to True, cache will be ignored
-#     :return: OrderedDict
-#     """
-#     if force or data_type not in _cache:
-#         _cache[data_type] = _read_csv(file)
-#     return _cache[data_type]
-#
-#
-# def clear_cache():
-#     for k in list(_cache.keys()):
-#         _cache.pop(k)
-#
-#
-# def get_statuses(force=False):
-#     return _get_data('statuses', STATUSES_FILE, force)
-#
-#
-# def get_boards(force=False):
-#     return _get_data('boards', BOARDS_FILE, force)
-#
-#
-# def get_cards(force=False):
-#     return _get_data('cards', CARDS_FILE, force)
+@database_common.connection_handler
+def change_card_status(cursor, card_id, board_id, status_id):
+    cursor.execute("""
+    UPDATE cards
+    SET status_id = %(status_id)s, board_id = %(board_id)s
+    WHERE id = %(card_id)s
+    """, {'card_id': card_id, 'board_id': board_id, 'status_id': status_id})
